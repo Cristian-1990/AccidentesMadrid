2d46123631e49b290979790bf40a36d2f2b25fee
Cristian <crisalvfer14@gmail.com>
2026-09-18 17:00:30 +0200

    docs: Añade la documentación de AccidenteError y DomainError

--- AccidentesMadrid.Back/Errors/AccidenteError.cs
+++ AccidentesMadrid.Back/Errors/AccidenteError.cs
@@ -2,20 +2,50 @@ using AccidentesMadrid.Back.Errors.Common;
 
 namespace AccidentesMadrid.Back.Errors;
 /// <summary>
-/// 
+/// Clase abstracta que hereda de DomainErro y maneja los errores de 
 /// </summary>
 /// <param name="Message"></param>
 public abstract record AccidenteError(string Message) : DomainError(Message)
 {
+    /// <summary>
+    /// Error de dominio para cuando el fichero csv no existe.
+    /// Detiene el programa por completo y no mostrará ningun resultado,
+    /// mostrará el error
+    /// </summary>
+    /// <param name="Rutas"></param>
     public sealed record FicheroNoEncontrado(IEnumerable<string> Rutas)
         : AccidenteError($"No se encontraron los siguientes ficheros: {string.Join(", ", Rutas)}");
-
+/// <summary>
+/// Analiza cada fila y si una fila tiene datos erroneos captura el error
+/// </summary>
+/// <param name="NumExpediente">Identifica que número de fila fue la que falló para poder encontrarla</param>
+/// <param name="Motivo">Explica porqué falló</param>
     public sealed record FilaInvalida(string NumExpediente, string Motivo)
         : AccidenteError($"Fila inválida ({NumExpediente}): {Motivo}");
 }
 
+/// <summary>
+/// Factory para el manejo de errores
+/// </summary>
+///
+///
+/// ================ AÑADIMOS PATRON FACTCTORY PARA MANEJO DE ERRORES================
+/// 
 public static class AccidenteErrors
 {
+    /// <summary>
+    /// Error para fichero no encontrado.
+    /// Salta el error si alguno de los 3 ficheros csv necesario para arrancar da fallo
+    /// </summary>
+    /// <param name="rutas">ruta donde se encuentra del fichero que no se encontró</param>
+    /// <returns>DomainErrror del tipo Fichero no encontrado</returns>
     public static DomainError FicheroNoEncontrado(IEnumerable<string> rutas) => new AccidenteError.FicheroNoEncontrado(rutas);
+    /// <summary>
+    /// Fallo de una fila del csv que no se puedo convertir al modelo
+    /// No detiene el programa, descarta la fila y sigue adelante.
+    /// </summary>
+    /// <param name="numExpediente">identificador de la fila que falló</param>
+    /// <param name="motivo">Explica el motivo de forma concreta</param>
+    /// <returns>DomainError del tipo FilaInvalilda</returns>
     public static DomainError FilaInvalida(string numExpediente, string motivo) => new AccidenteError.FilaInvalida(numExpediente, motivo);
 }
--- AccidentesMadrid.Back/Errors/Common/DomainError.cs
+++ AccidentesMadrid.Back/Errors/Common/DomainError.cs
@@ -1,9 +1,19 @@
 namespace AccidentesMadrid.Back.Errors.Common;
-
+/// <summary>
+/// Clase abstracta base que maneja cualquier error de dominio.
+/// Son las clases hijas las que definen el tipo de error
+/// </summary>
 public abstract record DomainError
 {
+    /// <summary>
+    /// Contiene el mensaje descriptivo del error
+    /// </summary>
     public string Message { get; init; } = string.Empty;
-
+/// <summary>
+/// Constructor de la clase.
+/// Protected para que solo las clases hijas puedan invocarlo
+/// </summary>
+/// <param name="message">Mensaje que describe el tipo de error</param>
     protected DomainError(string message)
     {
         Message = message;
