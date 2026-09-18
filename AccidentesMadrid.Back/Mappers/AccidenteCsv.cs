7de89e61cd4a0f675d5c5b104c0d0679a7c8c4b0
Cristian <crisalvfer14@gmail.com>
2026-09-18 18:07:01 +0200

    fit: Añadidas las clases AccidenteCsv para leer el CSV uy la libreria CsvHeper para leer y escribir ficheros(evita el uso de split)

---
+++ AccidentesMadrid.Back/Mappers/AccidenteCsv.cs
@@ -0,0 +1,67 @@
+using CsvHelper.Configuration.Attributes;
+
+namespace AccidentesMadrid.Back.Mappers;
+
+/// <summary>
+///Conecta una columna del Csv con la propiedad dónde debe  guardar el valor
+/// Acepta valores null porque ya limpiaremos esos campos en el mapper.
+/// </summary>
+public class AccidenteCsvRow
+{
+    [Name("num_expediente")]
+    public string? NumExpediente { get; set; }
+
+    [Name("fecha")]
+    public string? Fecha { get; set; }
+
+    [Name("hora")]
+    public string? Hora { get; set; }
+
+    [Name("localizacion")]
+    public string? Localizacion { get; set; }
+
+    [Name("numero")]
+    public string? Numero { get; set; }
+
+    [Name("cod_distrito")]
+    public string? CodDistrito { get; set; }
+
+    [Name("distrito")]
+    public string? Distrito { get; set; }
+
+    [Name("tipo_accidente")]
+    public string? TipoAccidente { get; set; }
+
+    [Name("estado_meteorológico")]
+    public string? EstadoMeteorologico { get; set; }
+
+    [Name("tipo_vehiculo")]
+    public string? TipoVehiculo { get; set; }
+
+    [Name("tipo_persona")]
+    public string? TipoPersona { get; set; }
+
+    [Name("rango_edad")]
+    public string? RangoEdad { get; set; }
+
+    [Name("sexo")]
+    public string? Sexo { get; set; }
+
+    [Name("cod_lesividad")]
+    public string? CodLesividad { get; set; }
+
+    [Name("lesividad")]
+    public string? Lesividad { get; set; }
+
+    [Name("coordenada_x_utm")]
+    public string? CoordenadaXUtm { get; set; }
+
+    [Name("coordenada_y_utm")]
+    public string? CoordenadaYUtm { get; set; }
+
+    [Name("positiva_alcohol")]
+    public string? PositivaAlcohol { get; set; }
+
+    [Name("positiva_droga")]
+    public string? PositivaDroga { get; set; }
+}
