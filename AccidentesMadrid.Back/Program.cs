5e7e8b3eadb0e2c59de5239eef286cd2ac6c5e0c
Cristian <Crisalvfer14@gmail.com>
2026-09-19 12:07:01 +0200

    feat: punto de entrada con medicion de tiempo de lectura de los ficheros, justifica la decision de usar paralelismo con un tiempo de lectura de archivo >50 ms (475ms)

--- AccidentesMadrid.Back/Program.cs
+++ AccidentesMadrid.Back/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using AccidentesMadrid.Back.Repositories;
+
+var repo = new AccidentesRepository();
+var stopwatch = Stopwatch.StartNew();
+
+var resultado = repo.LeerAccidentes(["data/Accidentes-2025.csv"]);
+
+stopwatch.Stop();
+
+Console.WriteLine($"Tiempo: {stopwatch.ElapsedMilliseconds} ms");
+Console.WriteLine($"Accidentes: {resultado.Value.Accidentes.Count}");
+Console.WriteLine($"Descartados: {resultado.Value.Descartados.Count}");
